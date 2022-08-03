105b1d6dab2aa7bf59bc20680b30350de81f3648
GlenMakoge <ekundimem@gmail.com>
2022-08-03 12:46:14 +0100

    calculator app

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+let currentResult = 0;
+const userFirstInput = 100;
+userFirstInput = 120;
+let operator = '+';
+let userSecondInput = 220;
+
+currentResult = userFirstInput + userSecondInput ;
+let result_description = `${userFirstInput} ${operator} ${userSecondInput}`
